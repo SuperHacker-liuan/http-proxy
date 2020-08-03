8fb7a218bec3e9378c58dadcab4545d3e2427b37
劉安 <liuan@sgcc.com.cn>
2020-08-03 17:26:53 +0800

    Deamonize

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use async_std::task;
+use daemonize::Daemonize;
 use std::error::Error;
 
 mod proxy;
@@ -6,5 +7,15 @@ mod proxy;
 type Result<T> = std::result::Result<T, Box<dyn Error>>;
 
 fn main() {
+    daemonize();
     task::block_on(proxy::run()).expect("EXIT ");
+}
+
+fn daemonize() {
+    Daemonize::new()
+        .pid_file(format!("/tmp/http-proxy@{}.pid", 5555))
+        .working_directory("/tmp")
+        .umask(0o777)
+        .start()
+        .expect("Failed to start as daemon");
 }
